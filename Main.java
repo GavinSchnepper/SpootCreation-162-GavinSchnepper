2e54a5fcddbb4172c8305118b98c9f846c7b0398
GavinSchnepper <142642251+GavinSchnepper@users.noreply.github.com>
2023-08-19 04:37:00 +0000

    Added description.

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
+//Program Name: Spoot Creation
+//Author: Gavin Schnepper
+//Created: August 15th, 2023
+//
+//Description: This is a project that contains a class with a
+//constructor and method to create a "Spoot" object and provide the
+//user with the details that apply to each of the objects.
+//When the project is loaded up information will be displayed relating
+//to the creation of a Spoot. It will provide the name, weight, age,
+//color and ability that each of the objects "Spoots" hold. A setter
+//method will be used which takes input from the user to change the
+//name of the Spoot and then a getter method can be used to verify that
+//the name provided has updated properly and maintains its new
+//description.
+//
+//
+//Modified: August 18th, 2023
+
+//Import scanner utility
 import java.util.Scanner;
 
 class Main{
