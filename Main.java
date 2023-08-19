d1f5dd98223203959428028741e5b847d63f718e
GavinSchnepper <142642251+GavinSchnepper@users.noreply.github.com>
2023-08-19 04:11:28 +0000

    Updated Getters and Setters

--- Main.java
+++ Main.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 class Main{
   public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
     Spoot spoot1 = new Spoot("Milo", "Black", 123.45, 100, "Devious");
     Spoot spoot2 = new Spoot("Zoey", "Grey", 100.1, 150, "Friendly");
     Spoot spoot3 = new Spoot("Tigger", "Orange", 150.5, 200, "Supportive");
 
     spoot1.details();
     spoot2.details();
+    spoot3.details();
+    
+    System.out.println("Re-Name the first Spoot!");
+    String givenName1 = input.nextLine();
+    spoot1.setName(givenName1);
+    spoot1.getName();
+    spoot1.details();
+
+    System.out.println("Re-Name the second Spoot!");
+    String givenName2 = input.nextLine();
+    spoot2.setName(givenName2);
+    spoot2.getName();
+    spoot2.details();
+
+    System.out.println("Re-Name the third Spoot!");
+    String givenName3 = input.nextLine();
+    spoot3.setName(givenName3);
+    spoot3.getName();
     spoot3.details();
   }
 }
@@ -24,10 +47,17 @@ class Spoot{
     color = spootColor;
     weight = spootWeight;
     age = spootAge;
+    ability = spootAbility;
   }
   
   void details(){
     System.out.println("My name is " + name + ". I am " + age + " years old." + 
                        " I am " + color + " and I weigh " + weight + " lbs.");
+  }
+  public void setName(String givenName){
+    this.name = givenName;
+  }
+  public void getName(){
+    System.out.println("This spoot has been Re-Named to " + name);
   }
 }
